87a1f2a5d3cb841d565c8139e0d487d7c5fd011e
fedyann <117765096+fedyann@users.noreply.github.com>
2024-04-24 18:37:43 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,121 +1,68 @@
-const config = {
-    'lineSize': 5,
-    'color': 'black'
-}
-
-window.onload = () => {
-
-    // Инициализируем html элементы
+window.onload = function() {
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
     const indicator = document.getElementById('indicator');
 
-    // Устанавливаем размер холста
-    canvas.setAttribute('width', window.innerWidth);
-    canvas.setAttribute('height', window.innerHeight);
-
-    // Инициализируем стиль кисти
-    ctx.lineWidth = config.lineSize;
-    ctx.lineJoin = 'round';
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = config.color;
-    ctx.fillStyle = config.color;
-
-    var isRec = false,
-        newDraw = false,
-        posX = [],
-        posY = []
-
-    // При нажатии на мышь
-    canvas.addEventListener("mousedown", (e) => {
-        if (isRec) return;
-        clearCanvas();
-        canvas.onmousemove = (e) => recordMousePos(e);
-    });
-
-    // Когда мышь отпущена
-    canvas.addEventListener("mouseup", () => stopDrawing());
-
-    // При нажатии на пробел
-    document.addEventListener("keydown", (e) => {
-        if(e.code == "Space") {
-            if(!isRec) {
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
-                ctx.beginPath();
-                isRec = true;
-                switchIndicator(false);
-                autoDraw();
-            }
-        }
-    })
-
-    // Добавляем позиции X и Y мыши в массимы arrayX и arrayY
-    function recordMousePos(e) {
-        posX.push(e.clientX);
-        posY.push(e.clientY);
-        drawLine(e.clientX, e.clientY);
+    document.ontouchmove = function(e){ e.preventDefault(); }
+    var canvastop = canvas.offsetTop
+  
+    var context = canvas.getContext("2d");
+  
+    var lastx;
+    var lasty;
+  
+    context.strokeStyle = "#000000";
+    context.lineCap = 'round';
+    context.lineJoin = 'round';
+    context.lineWidth = 5;
+  
+    function clear() {
+      context.fillStyle = "#ffffff";
+      context.rect(0, 0, 300, 300);
+      context.fill();
     }
-
-    // Рисование линий
-    function drawLine(x, y) {
-        ctx.lineTo(x, y);
-        ctx.stroke();
+  
+    function dot(x,y) {
+      context.beginPath();
+      context.fillStyle = "#000000";
+      context.arc(x,y,1,0,Math.PI*2,true);
+      context.fill();
+      context.stroke();
+      context.closePath();
     }
-
-    // Очистить холст
-    function clearCanvas() {
-        if(newDraw) {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            newDraw = false;
-            if(sketch != null) {
-                sketch.style.visibility = 'visible';
-            }
-        }
-        ctx.beginPath();
+  
+    function line(fromx,fromy, tox,toy) {
+      context.beginPath();
+      context.moveTo(fromx, fromy);
+      context.lineTo(tox, toy);
+      context.stroke();
+      context.closePath();
     }
-
-    // Остановка рисования
-    function stopDrawing() {
-        canvas.onmousemove = null;
-        posX.push(undefined);
-        posY.push(undefined);
+  
+    canvas.ontouchstart = function(event){                   
+      event.preventDefault();                 
+      
+      lastx = event.touches[0].clientX;
+      lasty = event.touches[0].clientY - canvastop;
+  
+      dot(lastx,lasty);
     }
-
-    // Изменить цвет индикатора
-    function switchIndicator(enable) {
-        if(enable) {
-            indicator.classList.add('isWrite');
-        }else {
-            indicator.classList.remove('isWrite');
-        }
+  
+    canvas.ontouchmove = function(event){                   
+      event.preventDefault();                 
+  
+      var newx = event.touches[0].clientX;
+      var newy = event.touches[0].clientY - canvastop;
+  
+      line(lastx,lasty, newx,newy);
+      
+      lastx = newx;
+      lasty = newy;
     }
-
-    // Автоматическое рисование
-    function autoDraw() {
-        var sketch = document.getElementById("sketch");
-        var x = posX;
-        var y = posY;
-
-        var drawing = setInterval(() => {
-            var currentX = x.shift();
-            var currentY = y.shift();
-            if (x.length <= 0 && y.length <= 0) {
-                clearInterval(drawing);
-                switchIndicator(true);
-                isRec = false;
-                newDraw = true;
-            }else {
-                if(currentX == undefined && currentY == undefined) {
-                    ctx.beginPath();
-                }else {
-                    drawLine(currentX, currentY);
-                }
-            }
-        }, 1);
-
-        if(sketch != null) {
-            sketch.style.visibility = 'hidden';
-        }
-    }
-    
-}
+  
+  
+    var clearButton = document.getElementById('clear')
+    clearButton.onclick = clear
+  
+    clear()
+  }
